3a0b17f166d098a420f3042b4c039f9c1779daca
Антон <antonL1790@yandex.ru>
2022-02-22 21:04:25 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homework5cource2/HomeWork5Cource2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework5cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork5Cource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork5Cource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homework5cource2/HomeWork5Cource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework5cource2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork5Cource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
